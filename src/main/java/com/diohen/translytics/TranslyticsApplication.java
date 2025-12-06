15a999d20382f8e1e099e696a77f12375c7b2822
Diohen <diogohss17@gmail.com>
2025-12-06 13:01:37 -0300

    Start Translytics project API

---
+++ src/main/java/com/diohen/translytics/TranslyticsApplication.java
@@ -0,0 +1,13 @@
+package com.diohen.translytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diohen/translytics/TranslyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diohen.translytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
